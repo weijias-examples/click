d6da61bd9ede99b212c0b78d4026c44a6e36e220
sun031 <weijia_sun@163.com>
2019-09-22 20:59:57 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ setup(
     install_requires=[
         'Click',
     ],
+    # please note the entry_points
+    # The magic is in the entry_points parameter. Below console_scripts, each line identifies one console script.
+    # The first part before the equals sign (=) is the name of the script that should be generated,
+    # the second part is the import path followed by a colon (:) with the Click command.
     entry_points={
         "console_scripts": [
             'sclick=s_click.main:run',
